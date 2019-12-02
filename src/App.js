8cc6d0eda6a733005c9e24af19cd82530b3636cf
Tara Sherman <taradelin@gmail.com>
2019-12-02 18:19:41 -0500

    adjusting links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
-import { withRouter, Route, Link } from 'react-router-dom';
-import { token } from "./utils/axiosWithAuth";
+import { withRouter, Switch, Route } from 'react-router-dom';
 import PrivateRoute from "./Components/PrivateRoute";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
@@ -10,18 +9,15 @@ import Header from "./Components/Header";
 import Footer from './Components/Footer';
 
 function App() {
-  const loggedIn = token;
 
   return (
     <div className="App">
-    <Header />
-    <nav>
-        {loggedIn && <Link to="/home">Home</Link>}
-      </nav>
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/" component={Login} />
-      <PrivateRoute exact path="/home" component={Home} />
-
+      <Header />
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/" component={Login} />
+          <PrivateRoute exact path="/home" component={Home} />
+        </Switch>
     <Footer />
     </div>
   );
